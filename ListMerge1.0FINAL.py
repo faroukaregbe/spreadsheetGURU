2782443194b2d1ea07f85c2d5b8aed32f136118d
faroukaregbe <faroukoluaregbe@gmail.com>
2017-04-25 22:48:30 -0500

    Here is my file to to excel list merges

---
+++ ListMerge1.0FINAL.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import csv
+
+
+susan = pd.read_csv('SteveNoMizzou.csv')
+math1300 = pd.read_csv('MASTERLISTsept30.csv')
+#print (math1300)
+#newlist = susan.merge(math1300)
+print(list(math1300))
+
+
+#newlist.to_csv('Outputtest1.csv')
+
+
+    
